8b6b0c14e5bcdc3dedf2484b8797750b7f0c92f3
Santosh Kumar <thecodeneverlie@gmail.com>
2026-03-17 21:56:20 +0530

    'done'

--- src/services/masterServices/user.service.ts
+++ src/services/masterServices/user.service.ts
@@ -1,6 +1,6 @@
 import { Types } from "mongoose";
 import { BadRequestError, NotFoundError } from "../../core/errors";
-import { signAccessToken,signRefreshToken } from "../../core/jwt";
+import { signAccessToken, signRefreshToken } from "../../core/jwt";
 import { IUser } from "../../interfaces/masterInterfaces/user.interface";
 import { courseModel } from "../../models/course.model";
 import RoleModel from "../../models/role.model";
@@ -26,26 +26,27 @@ export class UserService {
     const role = await RoleModel.findById(data.roleId);
     if (!role) throw new NotFoundError("Role not found");
 
-    const user=await userModel.create(data);
-    const accessToken = signAccessToken({userId: user._id});
-    const refreshToken = signRefreshToken({userId: user._id});
-    return{user,accessToken,refreshToken};
+    const user = await userModel.create(data);
+    const accessToken = signAccessToken({ userId: user._id });
+    const refreshToken = signRefreshToken({ userId: user._id });
+    return { user, accessToken, refreshToken };
   }
 
-  async login(data:Partial<IUser>){
-    const user=await userModel.findOne({email:data.email});
-    if(!user) throw new BadRequestError("Invalid Credentials");
-    const isPasswordValid=await user.comparePassword(data.password!);
-    if(!isPasswordValid){
-        throw new BadRequestError("Invalid credentials")
+  async login(data: Partial<IUser>) {
+    const user = await userModel.findOne({ email: data.email });
+    if (!user) throw new BadRequestError("Invalid Credentials");
+    const isPasswordValid = await user.comparePassword(data.password!);
+    if (!isPasswordValid) {
+      throw new BadRequestError("Invalid credentials");
     }
-    const accessToken=signAccessToken({userId:user._id})
-    const refreshToken=signRefreshToken({userId:user._id})
+    const accessToken = signAccessToken({ userId: user._id });
+    const refreshToken = signRefreshToken({ userId: user._id });
 
-    return{
-        user,accessToken,refreshToken
-    }
-
+    return {
+      user,
+      accessToken,
+      refreshToken,
+    };
   }
 
   async getUsers() {
@@ -58,18 +59,16 @@ export class UserService {
       .sort({ createdAt: -1 });
   }
 
-    async toggleUserStatus(id: string) {
-      const user = await userModel.findById(id);
-      if (!user) throw new NotFoundError("User not found");
-      user.status =
-        user.status === STATUS.ACTIVE
-          ? STATUS.INACTIVE
-          : STATUS.ACTIVE;
-      await user.save();
-      return user;
-    }
+  async toggleUserStatus(id: string) {
+    const user = await userModel.findById(id);
+    if (!user) throw new NotFoundError("User not found");
+    user.status =
+      user.status === STATUS.ACTIVE ? STATUS.INACTIVE : STATUS.ACTIVE;
+    await user.save();
+    return user;
+  }
 
-   async getUserById(id: string) {
+  async getUserById(id: string) {
     const user = await userModel
       .findOne({ _id: new Types.ObjectId(id), is_deleted: false })
       .populate("universityId")
@@ -80,13 +79,13 @@ export class UserService {
     return user;
   }
 
-   async updateUser(id: string, data: Partial<IUser>) {
+  async updateUser(id: string, data: Partial<IUser>) {
     const user = await userModel.findById(id);
     if (!user || user.isDeleted) throw new NotFoundError("User not found");
     if (data.email) {
       const emailExists = await userModel.findOne({
         email: data.email,
-        _id: { $ne: id }
+        _id: { $ne: id },
       });
       if (emailExists) {
         throw new BadRequestError("Email already exists");
@@ -95,17 +94,14 @@ export class UserService {
     if (data.phone) {
       const phoneExists = await userModel.findOne({
         phone: data.phone,
-        _id: { $ne: id }
+        _id: { $ne: id },
       });
       if (phoneExists) {
         throw new BadRequestError("Phone already exists");
       }
     }
-    return await userModel.findByIdAndUpdate(
-      id,
-      data,
-      { new: true }
-    )
+    return await userModel
+      .findByIdAndUpdate(id, data, { new: true })
       .populate("universityId")
       .populate("courseId")
       .populate("semesterId")
@@ -122,8 +118,8 @@ export class UserService {
     return user;
   }
 
-  async deleteUser(id:string){
-    const result =await userModel.findByIdAndDelete(id);
+  async deleteUser(id: string) {
+    const result = await userModel.findByIdAndDelete(id);
     return result;
   }
 }
