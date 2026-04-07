4424de78ac246fcfea76272713222f7f62bc721b
Santosh Kumar <thecodeneverlie@gmail.com>
2026-04-07 13:22:40 +0530

    'done'

--- src/routes/masterRoutes/students.routes.ts
+++ src/routes/masterRoutes/students.routes.ts
@@ -5,6 +5,8 @@ const studentRouter = Router();
 const studentController = new StudentController();
 
 
+studentRouter.get("/my-university", studentController.getStudentsByMyUniversity);
+studentRouter.get("/my-university/alumini", studentController.getAluminiByMyUniversity);
 studentRouter.get("/", studentController.getAllStudents);
 
 export default studentRouter;
--- src/services/students.service.ts
+++ src/services/students.service.ts
@@ -19,4 +19,48 @@ export class StudentService {
 
     return students;
   };
+
+  // --- GET STUDENTS BY UNIVERSITY ---
+  getStudentsByUniversity = async (universityId: string, authUserId: string) => {
+    const studentRole = await RoleModel.findOne({ name: "STUDENT", isDeleted: false });
+    
+    if (!studentRole) {
+      throw new Error("Student role not found. Please ensure the 'STUDENT' role exists in the database.");
+    }
+
+    const students = await userModel.find({ 
+      roleId: studentRole._id, 
+      universityId: universityId,
+      _id: { $ne: authUserId },
+      isDeleted: false 
+    })
+    .populate("universityId")
+    .populate("courseIds")
+    .populate("semesterId")
+    .select("-password");
+
+    return students;
+  };
+
+
+  getAluminiByUniversity = async (universityId: string, authUserId: string) => {
+    const studentRole = await RoleModel.findOne({ name: "ALUMINI", isDeleted: false });
+    
+    if (!studentRole) {
+      throw new Error("Alumini role not found. Please ensure the 'ALUMINI' role exists in the database.");
+    }
+
+    const students = await userModel.find({ 
+      roleId: studentRole._id, 
+      universityId: universityId,
+      _id: { $ne: authUserId },
+      isDeleted: false 
+    })
+    .populate("universityId")
+    .populate("courseIds")
+    .populate("semesterId")
+    .select("-password");
+
+    return students;
+  };
 }
