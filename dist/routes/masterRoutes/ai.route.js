d4b3657a2c01d1d1abedd4645d032be8c2cff964
Santosh Kumar <thecodeneverlie@gmail.com>
2026-05-02 15:54:00 +0530

    fix: resolve deployment conflict by removing tracked secrets/uploads and adding resilient pull logic

--- dist/routes/masterRoutes/ai.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authMiddleware_1 = require("../../middlewares/authMiddleware");
-const ai_controller_1 = require("../../controllers/ai.controller");
-const aiController = new ai_controller_1.AiController();
-const Airouter = (0, express_1.Router)();
-Airouter.post("/generate", authMiddleware_1.middleware, aiController.generateAIResponse);
-exports.default = Airouter;
